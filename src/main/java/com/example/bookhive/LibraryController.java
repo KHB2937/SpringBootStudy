0e37b9e8c7c12266d90a0635db5939640acad8d4
KHB2937 <gmlqja7913@gmail.com>
2023-05-02 15:37:56 +0900

    Initial commit

---
+++ src/main/java/com/example/bookhive/LibraryController.java
@@ -0,0 +1,13 @@
+package com.example.bookhive;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class LibraryController {
+    @GetMapping("/") // GET 방식으로 "path"를 호출했을 때, 어떠한 응답을 줄지
+    public String home(Model model) {
+        // model <= 페이지로 전달할 데이터들의 묶음
+        model.addAttribute("pageTitle", "Bookhive Library");
+        return "home"; // templates -> home.html
+    }
+}
