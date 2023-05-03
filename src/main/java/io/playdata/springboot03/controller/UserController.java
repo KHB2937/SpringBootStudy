ba38e8e81fa145ab0099811dafbb6532542ac4ad
qus0in <qus0in@gmail.com>
2023-05-03 14:45:37 +0900

    Service, update.

--- src/main/java/io/playdata/springboot03/controller/UserController.java
+++ src/main/java/io/playdata/springboot03/controller/UserController.java
@@ -2,6 +2,7 @@ package io.playdata.springboot03.controller;
 
 import io.playdata.springboot03.model.User;
 import io.playdata.springboot03.repository.UserRepository;
+import io.playdata.springboot03.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,16 +14,27 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
     @Autowired
-    private UserRepository userRepository;
+    UserService userService;
 
-    @GetMapping("")
+    @GetMapping("") // http://localhost:8080/users
     public List<User> getUsers() {
-        return userRepository.findAll();
+//        return userRepository.findAll();
+        return userService.getUsers();
     }
 
-    @PostMapping("")
+    @PostMapping("") // http://localhost:8080/users -> body : User
     public ResponseEntity<User> createUser(@RequestBody User user) {
-        User createdUser = userRepository.save(user);
+//        User createdUser = userRepository.save(user);
+        User createdUser = userService.createUser(user);
         return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{userId}") // http://localhost:8080/users/1 -> body : User
+    public ResponseEntity<User> updateUser(@PathVariable Long userId, @RequestBody User user) {
+        User updatedUser = userService.update(userId, user);
+        if (updatedUser == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND); // 값이 없음을 나타내는 응답
+        }
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
     }
 }
