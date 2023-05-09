401893f4a0db161341d28484a3ded77396254eb4
qus0in <qus0in@gmail.com>
2023-05-09 09:27:51 +0900

    Initial commit

---
+++ src/main/java/io/playdata/tshirts/TshirtsApplication.java
@@ -0,0 +1,13 @@
+package io.playdata.tshirts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TshirtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TshirtsApplication.class, args);
+	}
+
+}
