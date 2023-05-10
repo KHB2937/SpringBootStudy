eec27257c2f4972012cc0aa5b27ede373ca4ba7c
qus0in <qus0in@gmail.com>
2023-05-10 17:02:46 +0900

    로그인 구현

--- src/main/java/io/playdata/login/controller/AccountController.java
+++ src/main/java/io/playdata/login/controller/AccountController.java
@@ -1,8 +1,15 @@
 package io.playdata.login.controller;
 
+import io.playdata.login.model.Account;
+import io.playdata.login.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping // ('/')
@@ -13,5 +20,37 @@ public class AccountController {
     @GetMapping
     public String home() {
         return "index";
+    }
+
+    @GetMapping("/join")
+    public String joinPage(Model model) {
+        model.addAttribute("account", new Account());
+        return "join";
+    }
+
+    @Autowired
+    private AccountService accountService;
+
+    @PostMapping("/join")
+    public String join(Model model,
+                       @ModelAttribute("account") Account account,
+                       RedirectAttributes redirectAttributes) {
+        accountService.join(account);
+        redirectAttributes.addFlashAttribute("msg", "정상 가입 완료");
+        return "redirect:/";
+    }
+
+    @PostMapping("/login")
+    public String login(HttpSession session,
+                        HttpServletResponse response,
+                        RedirectAttributes redirectAttributes,
+                        @RequestParam("loginID") String loginID,
+                        @RequestParam("password") String password) {
+        Account account = accountService.login(loginID, password);
+        if (account == null) {
+            redirectAttributes.addFlashAttribute("msg", "존재하지 않는 회원입니다");
+            return "redirect:/";
+        }
+        return "main";
     }
 }
