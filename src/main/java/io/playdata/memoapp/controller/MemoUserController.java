93f6f975f9b50ad4b70b9e04dbafbb091c1e9a89
qus0in <qus0in@gmail.com>
2023-05-11 10:52:49 +0900

    join.html

--- src/main/java/io/playdata/memoapp/controller/MemoUserController.java
+++ src/main/java/io/playdata/memoapp/controller/MemoUserController.java
@@ -1,8 +1,10 @@
 package io.playdata.memoapp.controller;
 
+import io.playdata.memoapp.model.MemoUserDTO;
 import io.playdata.memoapp.service.MemoUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -20,4 +22,13 @@ public class MemoUserController {
         return "login"; // prefix -> (templates/)login(.html) <- suffix
     }
 
+    // 가입 페이지 (/join) -> GetMapping
+    @GetMapping("/join")
+//    public String join() {
+    public String join(Model model) {
+        // TODO : 로그인이 되었을 때 -> 메인으로 돌려주는.
+        // 폼 입력할 때 입력을 받을 객체 전달 (user)
+        model.addAttribute("user", new MemoUserDTO());
+        return "join"; // join.html
+    }
 }
