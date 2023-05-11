af43408cd85c0244f8b109485edf26030fa0f099
qus0in <qus0in@gmail.com>
2023-05-11 09:52:41 +0900

    Model (Private)

--- src/main/java/io/playdata/memoapp/model/MemoUserDTO.java
+++ src/main/java/io/playdata/memoapp/model/MemoUserDTO.java
@@ -12,11 +12,11 @@ public class MemoUserDTO {
     // ID : User들간의 구분을 위한 식별자 (유저별로 1씩 존재)
     @Id // JPA
     @GeneratedValue // 1씩 증가시켜주겠다 (JPA)
-    Long id;
+    private Long id;
     // 로그인용 인증정보
-    String loginId;
-    String password;
+    private String loginId;
+    private String password;
     // 개별 정보
-    String name;
-    String image; // 프로필 이미지 (파일을 업로드 -> 파일명)
+    private String name;
+    private String image; // 프로필 이미지 (파일을 업로드 -> 파일명)
 }
