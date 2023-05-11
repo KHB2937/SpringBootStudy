d07d05492ae42cc0c5d23de7e26e4bfccd4e520c
qus0in <qus0in@gmail.com>
2023-05-11 11:55:03 +0900

    session

--- src/main/java/io/playdata/memoapp/controller/MemoUserController.java
+++ src/main/java/io/playdata/memoapp/controller/MemoUserController.java
@@ -8,6 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 @Controller // 스프링에 Controller임을 등록
 @RequestMapping // @RequestMapping("/") -> 기본 index 경로, 루트 ('/') 경로에 이 컨트롤러를 연결
 public class MemoUserController {
@@ -16,8 +20,25 @@ public class MemoUserController {
     private MemoUserService memoUserService;
 
     @GetMapping("/") // http://localhost:8080/
-    public String index() { // String -> 연결할 view의 이름
-        // TODO : 로그인이 되었을 때
+    public String index(
+            HttpSession session,
+            Model model,
+            // Cookie cookie = new Cookie("name", user.getName());
+            @CookieValue("name") String name) { // String -> 연결할 view의 이름
+        // 로그인이 되었을 때
+        if (session != null) { // session == null
+            // 1) 쿠키에 저장된 session ID를 날려버렸을 때
+            // 2) 애초에 없었을 때
+            // 세션 안에서는 모든 타입이 Object
+            Object login = session.getAttribute("login");
+            if (login != null) { // set 안해준 attribute는 null 취급
+                // session은 있는데 login을 안한 거
+                if ((boolean) login) { // 형변환
+                    model.addAttribute("msg", name + "님의 로그인을 환영합니다");
+                    return "main"; // forward
+                }
+            }
+        }
         // 로그인이 안 되었을 때
         return "login"; // prefix -> (templates/)login(.html) <- suffix
     }
@@ -57,14 +78,25 @@ public class MemoUserController {
             @RequestParam("loginId") String loginId, // <input name="loginId" type="text">
             @RequestParam("password") String password, // <input name="loginId" type="text">
             Model model,
-            RedirectAttributes redirectAttributes
+            RedirectAttributes redirectAttributes,
+            HttpSession session, // session을 다뤄주는 것
+            HttpServletResponse response // cookie 관련
     ) {
         MemoUserDTO user = memoUserService.login(loginId, password);
+        // 로그인 실패
         if (user == null) {
             redirectAttributes.addFlashAttribute("msg", "인증 정보가 올바르지 않습니다");
             return "redirect:/";
         }
-        model.addAttribute("msg", user.getName() + "님의 로그인을 환영합니다");
-        return "main";
+        // 로그인 성공
+//        model.addAttribute("msg", user.getName() + "님의 로그인을 환영합니다");
+        // -> redriect라서 작동 안하는 코드
+        session.setAttribute("login", true); // login 여부를 true 바꾸고, 그 세션을 서버 저장
+        Cookie cookie = new Cookie("name", user.getName());
+        cookie.setMaxAge(60 * 60); // 1시간 (1초 단위)
+        response.addCookie(cookie);
+        // db 같은 저장 (Spring Session) -> cookie : Session ID
+//        return "main";
+        return "redirect:/"; // index
     }
 }
