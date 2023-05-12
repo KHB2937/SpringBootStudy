d9c0c756058d9dffb7b0d56625c56430da6ef3b4
qus0in <qus0in@gmail.com>
2023-05-12 15:13:08 +0900

    최종 커밋

--- src/main/java/io/playdata/security/confg/SecurityConfig.java
+++ src/main/java/io/playdata/security/confg/SecurityConfig.java
@@ -8,12 +8,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration // 설정을 등록해주는 어노테이션
 @EnableWebSecurity // 웹 보안 설정
