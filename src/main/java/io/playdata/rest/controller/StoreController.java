cad6104827db80e87f19c357d5c44e54b10595c0
KHB2937 <gmlqja7913@gmail.com>
2023-05-15 16:50:02 +0900

    가계

--- src/main/java/io/playdata/rest/controller/StoreController.java
+++ src/main/java/io/playdata/rest/controller/StoreController.java
@@ -1,2 +1,74 @@
-package io.playdata.rest.controller;public class StoreController {
+package io.playdata.rest.controller;
+
+import io.playdata.rest.model.StoreDTO;
+import io.playdata.rest.service.StoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController // Controller <-> RestController
+@RequestMapping("/store")
+public class StoreController {
+    @Autowired
+    private StoreService storeService;
+
+    // C.R.U.D
+    // Create - Post
+//    @PostMapping("") // 생성이 되었을 때 생성된 데이터를 반환
+    @PostMapping // 생성이 되었을 때 생성된 데이터를 반환
+    // Body -> 요청을 받고, 반환
+    public StoreDTO createStore(@RequestBody StoreDTO store) {
+        return storeService.createStore(store);
+    }
+
+    // Read - Read (one) / Read All / search, filtering
+//    @GetMapping // ("")
+//    public List<StoreDTO> findAllStore() {
+//        return storeService.findAll();
+//    }
+
+    @GetMapping
+    public List<StoreDTO> findAllStoreSortByPrice(@RequestParam(required = false) Boolean price) {
+        // true은 오름차순으로, false은 내림차순으로
+        if (price == null) {
+            return storeService.findAll();
+        }
+        return storeService.findAll(price);
+    }
+
+    @GetMapping("/{id}") // id <- 경로에서 ID 받기
+    public Optional<StoreDTO> findById(@PathVariable Long id) {
+        return storeService.findById(id);
+    }
+
+    @GetMapping("/one") // query parameter로 ID 받기 // one?id=1
+    public Optional<StoreDTO> findByIdQuery(@RequestParam Long id) {
+        return storeService.findById(id);
+    }
+
+    @GetMapping("/search/{kwd}")
+    public List<StoreDTO> findByKwd(@PathVariable String kwd) {
+        return storeService.findByNameKwd(kwd);
+    }
+
+    // Delete
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Long id) {
+        storeService.deleteById(id);
+    }
+
+    // Put -> 새로운 걸 넣어서 완전히 대체하겠다
+    @PutMapping("/{id}")
+    public StoreDTO updateById(@PathVariable Long id, @RequestBody StoreDTO store) {
+        // Put으로 넣으면 데이터 전체를 넣어서 한 번 바꾸는게 규약
+        return storeService.updateById(id, store);
+    }
+
+    @PatchMapping("/{id}/price")
+    public StoreDTO changePriceById(@PathVariable Long id, @RequestParam int price) {
+        // 하나의 속성만 바꾸는게 규약
+        return storeService.changePriceById(id, price);
+    }
 }
--- src/main/java/io/playdata/rest/repository/StoreRepository.java
+++ src/main/java/io/playdata/rest/repository/StoreRepository.java
@@ -1,2 +1,16 @@
-package io.playdata.rest.repository;public interface StoreRepository {
+package io.playdata.rest.repository;
+
+import io.playdata.rest.model.StoreDTO;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StoreRepository extends JpaRepository<StoreDTO, Long> {
+    List<StoreDTO> findByNameContaining(String kwd);
+
+    List<StoreDTO> findAllByOrderByPriceAsc();
+
+    List<StoreDTO> findAllByOrderByPriceDesc();
 }
